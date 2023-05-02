ca203f09bf48ebcdd9fce3473dbf1725f6e3b830
raj073 <rajoni.das@outlook.com>
2023-05-02 13:41:03 +0600

    Layout and Sidebar updated

--- src/Component/SIdebar/Sidebar.js
+++ src/Component/SIdebar/Sidebar.js
@@ -1,7 +1,207 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="w-full p-4">
+      <nav className="space-y-4 text-sm">
+        <div className="space-y-1">
+          <h2 className="text-xs font-bold">Category</h2>
+          <div className="flex flex-col font-medium text-xs space-y-1 ps-3">
+            <Link to="/">Full Avatar</Link>
+            <Link to="/">Others</Link>
+          </div>
+        </div>
+        <div className="space-y-1">
+          <h2 className="text-xs font-bold">Contents</h2>
+          <div className="flex flex-col font-medium text-xs space-y-1 ps-3">
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                VRChat(Quest)
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                VRChat(PCVR)
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                Others
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-1">
+          <h2 className="text-xs font-bold">Price</h2>
+          <div className="flex flex-col font-medium text-xs space-y-1 ps-3">
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                Under $10
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                $10 to $20
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                $20 to $30
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                $30 to $40
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                $40 to $50
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                $50 to $70
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                $70 & above
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-1">
+          <h2 className="text-xs font-bold">Polygon amount</h2>
+          <div className="flex flex-col font-medium text-xs space-y-1 ps-3">
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                Under △7,500
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                △7,500 to △7,500
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                △10,000 to △15,000
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                △15,000 to △20,000
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                △20,000 to △32,000
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                △32,000 to △70,000
+              </Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link to="/" className="ps-[6px]">
+                △70,000 & above
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-1">
+          <h2 className="text-xs font-bold">Auto Upload Support</h2>
+          <div className="flex flex-col font-medium text-xs space-y-1 ps-3">
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link className="ps-[6px]">Supported</Link>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                className="border-gray-[#6A6A6A] rounded-sm h-3 w-3"
+              />
+              <Link className="ps-[6px]">Unsupported</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -7,10 +7,36 @@ import Sidebar from "../Component/SIdebar/Sidebar";
 const Main = () => {
   return (
     <div>
-      <Header></Header>
+      {/* <Header></Header>
       <Sidebar></Sidebar>
       <Outlet></Outlet>
-      <Footer></Footer>
+      <Footer></Footer> */}
+
+      <div class="flex">
+        <div class="flex-1 flex flex-col">
+          <header>
+            <Header></Header>
+          </header>
+          <div class="flex">
+            <nav class="flex w-64 h-[775px]">
+              <div class="w-full flex mx-auto">
+                <div class="">
+                  <Sidebar></Sidebar>
+                </div>
+              </div>
+            </nav>
+            <main class="flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14">
+              <div class="flex w-full mx-auto px-6 py-6">
+                <div class="flex flex-col w-full h-full text-gray-900">
+                  <div class="w-full mx-auto">
+                    <Outlet></Outlet>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
