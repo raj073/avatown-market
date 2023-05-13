76f121baa342d05b50118f3c79881f3ae76e20cf
raj073 <rajoni.das@outlook.com>
2023-05-13 21:25:47 +0600

    Main Layout updated

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../Component/Footer/Footer";
 import Header from "../Component/Header/Header";
 import Sidebar from "../Component/SIdebar/Sidebar";
 
 const Main = () => {
   return (
     <div>
-      {/* <Header></Header>
-      <Sidebar></Sidebar>
-      <Outlet></Outlet>
-      <Footer></Footer> */}
-
       <div className="flex">
         <div className="flex-1 flex flex-col">
           <header>
